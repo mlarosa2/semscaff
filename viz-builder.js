dcf68161805c7903167da90bc5eaa821a5b3527e
Mark LaRosa <mark.larosa2@gmail.com>
2017-04-05 11:04:47 -0400

    fix issue wit template spcacing

--- viz-builder.js
+++ viz-builder.js
@@ -277,167 +277,167 @@ const asChart = `${vizName.toLowerCase().split('-')[0]}Chart`;
 if (isJV) {
     directive = 
     `(function () {
-        'use strict';
-        /**
-         * @name ${vizName}
-         * @desc directive for creating and visualizing a ${createWidgetName(vizName)} Chart
-         */
+    'use strict';
+    /**
+     * @name ${vizName}
+     * @desc directive for creating and visualizing a ${createWidgetName(vizName)} Chart
+     */
 
-        angular.module("app.${vizName}.directive", [])
-            .directive("${camelCaseViz}", ${camelCaseViz});
-        
-        ${camelCaseViz}.$inject = ['$compile', 'VIZ_COLORS', '$filter', 'dataService'];
-
-        function ${camelCaseViz}($compile, VIZ_COLORS, $filter, dataService) {
-            ${camelCaseViz}Link.$inject = ['scope', 'ele', 'attrs', 'ctrl'];
-            ${camelCaseViz}Ctrl.$inject = ['$scope'];
-            return {
-                restrict: 'A',
-                require: ['?chart', '?cleanChart'],
-                priority: 300,
-                link: ${camelCaseViz}Link,
-                controller: ${camelCaseViz}Ctrl
-            };
-
-            function ${camelCaseViz}Link(scope, ele, attrs, ctrl) {
-                scope.chartCtrl = ctrl[0] ? ctrl[0] : ctrl[1];
-
-                /****************Data Functions*************************/
-                scope.chartCtrl.dataProcessor = dataProcessor;
-                /****************Tool Functions*************************/
-                scope.chartCtrl.highlightSelectedItem = highlightSelectedItem;
-                scope.chartCtrl.resizeViz = resizeViz;
-
-                //declare and initialize local variables
-                var ${asChart},
-                    uriData,
-                    html = '<div id=' + scope.chartCtrl.chartName + "-append-viz" + '><div id=' + scope.chartCtrl.chartName + '></div></div>';
-                
-                ele.append($compile(html)(scope));
-
-                /****************Data Functions*************************/
-
-                /**dataProcessor gets called from chart and is where the data manipulation happens for the viz
-                 *
-                 * @param newData
-                 */
-                function dataProcessor(newData) {
-                    var localChartData = JSON.parse(JSON.stringify(newData));
-                    scope.chartCtrl.chartDiv.attr('class', 'chart-div absolute-size');
-
-                    //return and alert the user if no data exists
-                    if (!localChartData.viewData) {
-                        console.log("No data returned from the backend");
-                        return;
-                    }
-
-                    //uriData is used for related insights
-                    uriData = localChartData.data;
-
-                    //filter dataTable
-                    for (var k in localChartData.dataTableAlign) {
-                        localChartData.dataTableAlign[k] = $filter("shortenAndReplaceUnderscores")(localChartData.dataTableAlign[k]);
-                    }
-
-                    var tipConfig = {
-                        type: "simple"
-                    };
-
-                    //create jv chart object
-                    ${asChart} = new jvCharts({
-                        type: "${vizName.toLowerCase().split('-')[0]}",
-                        name: scope.chartCtrl.chartName,
-                        options: localChartData.uiOptions,
-                        chartDiv: scope.chartCtrl.chartDiv,
-                        tipConfig: tipConfig,
-                        localCallbackRelatedInsights: relatedInsights,
-                        localCallbackRemoveHighlight: removeHighlight,
-                        setData: {
-                            data: localChartData.viewData,
-                            dataTable: localChartData.dataTableAlign,
-                            dataTableKeys: localChartData.dataTableKeys,
-                            colors: VIZ_COLORS.COLOR_SEMOSS
-                        },
-                        paint: true
-                    });
-                    //save colors to state
-                    if (ctrl[0]) {
-                        dataService.setState(localChartData.layout, { color: ${asChart}.data.color });
-                    }
-                    update();
-                }
-
-                /****************Update Functions*************************/
-                function update() {
-                    scope.chartCtrl.jvChart = ${asChart};
-                }
-
-                function resizeViz() {
-                    ${asChart}.chartDiv = scope.chartCtrl.chartDiv;
-                    ${asChart}.${asChart}.paint(${asChart});
-                    scope.chartCtrl.initializeModes();
-                }
-
-                function relatedInsights() {}
-
-                /****************Tool Functions*************************/
-                function highlightSelectedItem(selectedItems) {}
-
-                function removeHighlight() {}
-
-                //when directive ends, make sure to clean out excess listeners and dom elements outside of the scope
-                scope.$on("$destroy", function () {
-
-                });
-            }
-        } 
-
-        function  ${camelCaseViz}Ctrl($scope) {}
+    angular.module("app.${vizName}.directive", [])
+        .directive("${camelCaseViz}", ${camelCaseViz});
     
-    })();`;
+    ${camelCaseViz}.$inject = ['$compile', 'VIZ_COLORS', '$filter', 'dataService'];
+
+    function ${camelCaseViz}($compile, VIZ_COLORS, $filter, dataService) {
+        ${camelCaseViz}Link.$inject = ['scope', 'ele', 'attrs', 'ctrl'];
+        ${camelCaseViz}Ctrl.$inject = ['$scope'];
+        return {
+            restrict: 'A',
+            require: ['?chart', '?cleanChart'],
+            priority: 300,
+            link: ${camelCaseViz}Link,
+            controller: ${camelCaseViz}Ctrl
+        };
+
+        function ${camelCaseViz}Link(scope, ele, attrs, ctrl) {
+            scope.chartCtrl = ctrl[0] ? ctrl[0] : ctrl[1];
+
+            /****************Data Functions*************************/
+            scope.chartCtrl.dataProcessor = dataProcessor;
+            /****************Tool Functions*************************/
+            scope.chartCtrl.highlightSelectedItem = highlightSelectedItem;
+            scope.chartCtrl.resizeViz = resizeViz;
+
+            //declare and initialize local variables
+            var ${asChart},
+                uriData,
+                html = '<div id=' + scope.chartCtrl.chartName + "-append-viz" + '><div id=' + scope.chartCtrl.chartName + '></div></div>';
+            
+            ele.append($compile(html)(scope));
+
+            /****************Data Functions*************************/
+
+            /**dataProcessor gets called from chart and is where the data manipulation happens for the viz
+             *
+             * @param newData
+             */
+            function dataProcessor(newData) {
+                var localChartData = JSON.parse(JSON.stringify(newData));
+                scope.chartCtrl.chartDiv.attr('class', 'chart-div absolute-size');
+
+                //return and alert the user if no data exists
+                if (!localChartData.viewData) {
+                    console.log("No data returned from the backend");
+                    return;
+                }
+
+                //uriData is used for related insights
+                uriData = localChartData.data;
+
+                //filter dataTable
+                for (var k in localChartData.dataTableAlign) {
+                    localChartData.dataTableAlign[k] = $filter("shortenAndReplaceUnderscores")(localChartData.dataTableAlign[k]);
+                }
+
+                var tipConfig = {
+                    type: "simple"
+                };
+
+                //create jv chart object
+                ${asChart} = new jvCharts({
+                    type: "${vizName.toLowerCase().split('-')[0]}",
+                    name: scope.chartCtrl.chartName,
+                    options: localChartData.uiOptions,
+                    chartDiv: scope.chartCtrl.chartDiv,
+                    tipConfig: tipConfig,
+                    localCallbackRelatedInsights: relatedInsights,
+                    localCallbackRemoveHighlight: removeHighlight,
+                    setData: {
+                        data: localChartData.viewData,
+                        dataTable: localChartData.dataTableAlign,
+                        dataTableKeys: localChartData.dataTableKeys,
+                        colors: VIZ_COLORS.COLOR_SEMOSS
+                    },
+                    paint: true
+                });
+                //save colors to state
+                if (ctrl[0]) {
+                    dataService.setState(localChartData.layout, { color: ${asChart}.data.color });
+                }
+                update();
+            }
+
+            /****************Update Functions*************************/
+            function update() {
+                scope.chartCtrl.jvChart = ${asChart};
+            }
+
+            function resizeViz() {
+                ${asChart}.chartDiv = scope.chartCtrl.chartDiv;
+                ${asChart}.${asChart}.paint(${asChart});
+                scope.chartCtrl.initializeModes();
+            }
+
+            function relatedInsights() {}
+
+            /****************Tool Functions*************************/
+            function highlightSelectedItem(selectedItems) {}
+
+            function removeHighlight() {}
+
+            //when directive ends, make sure to clean out excess listeners and dom elements outside of the scope
+            scope.$on("$destroy", function () {
+
+            });
+        }
+    } 
+
+    function  ${camelCaseViz}Ctrl($scope) {}
+
+})();`;
 } else {
     directive = 
     `(function () {
-        'use strict';
-        /**
-         * @name ${vizName}
-         * @desc directive for creating and visualizing a ${createWidgetName(vizName)} Chart
-         */
+    'use strict';
+    /**
+     * @name ${vizName}
+     * @desc directive for creating and visualizing a ${createWidgetName(vizName)} Chart
+     */
 
-        angular.module("app.${vizName}.directive", [])
-            .directive("${camelCaseViz}", ${camelCaseViz});
-        
-        ${camelCaseViz}.$inject = ['$filter'];
+    angular.module("app.${vizName}.directive", [])
+        .directive("${camelCaseViz}", ${camelCaseViz});
+    
+    ${camelCaseViz}.$inject = ['$filter'];
 
-        function ${camelCaseViz}($filter) {
-            ${camelCaseViz}Link.$inject = ['scope', 'ele', 'attrs', 'ctrl'];
+    function ${camelCaseViz}($filter) {
+        ${camelCaseViz}Link.$inject = ['scope', 'ele', 'attrs', 'ctrl'];
 
-            return {
-                restrict: 'A',
-                require: ['chart'],
-                priority: 300,
-                link: ${camelCaseViz}Link
-            };
+        return {
+            restrict: 'A',
+            require: ['chart'],
+            priority: 300,
+            link: ${camelCaseViz}Link
+        };
 
-            function ${camelCaseViz}Link(scope, ele, attrs, ctrl) {
-                scope.chartCtrl = ctrl[0];
+        function ${camelCaseViz}Link(scope, ele, attrs, ctrl) {
+            scope.chartCtrl = ctrl[0];
 
-                var html = '<div style="overflow:hidden" class="append-viz" id=' + scope.chartCtrl.chartName + "-append-viz" + '><div class="absolute-size" id=' + scope.chartCtrl.chartName + '></div></div>';
-                ele.append(html);
+            var html = '<div style="overflow:hidden" class="append-viz" id=' + scope.chartCtrl.chartName + "-append-viz" + '><div class="absolute-size" id=' + scope.chartCtrl.chartName + '></div></div>';
+            ele.append(html);
 
-                scope.chartCtrl.dataProcessor = function (data) {};
-                scope.chartCtrl.highlightSelectedItem = function (selectedItem) {};
-                scope.chartCtrl.resizeViz = function () {};
+            scope.chartCtrl.dataProcessor = function (data) {};
+            scope.chartCtrl.highlightSelectedItem = function (selectedItem) {};
+            scope.chartCtrl.resizeViz = function () {};
 
-                function update(updateData) {}
+            function update(updateData) {}
 
-                //clean up
-                scope.$on('$destroy', function () {
+            //clean up
+            scope.$on('$destroy', function () {
 
-                });
-            }
+            });
         }
-    })();`;
+    }
+})();`;
 }
 
 fs.writeFile(`${vizName}.directive.js`, directive, (err) => {
@@ -450,54 +450,54 @@ fs.writeFile(`${vizName}.directive.js`, directive, (err) => {
 if (additionalTools) {
     let toolsDirective = 
     `(function () {
-        'use strict';
+    'use strict';
 
-        /**
-         * @name ${vizName}
-         * @desc ${createWidgetName(vizName)} additional tools
-         */
+    /**
+     * @name ${vizName}
+     * @desc ${createWidgetName(vizName)} additional tools
+     */
 
-        angular.module('app.${vizName}-tools.directive', [])
-            .directive('${camelCaseViz}Tools', ${camelCaseViz}Tools);
-        
-        ${camelCaseViz}Tools.$inject = [];
+    angular.module('app.${vizName}-tools.directive', [])
+        .directive('${camelCaseViz}Tools', ${camelCaseViz}Tools);
+    
+    ${camelCaseViz}Tools.$inject = [];
 
-        function ${camelCaseViz}Tools() {
-            ${camelCaseViz}ToolsCtrl.$inject = ['$rootScope', '$scope', 'dataService'];
-            ${camelCaseViz}ToolsLink.$inject = ['scope', 'ele', 'attrs', 'ctrl'];
+    function ${camelCaseViz}Tools() {
+        ${camelCaseViz}ToolsCtrl.$inject = ['$rootScope', '$scope', 'dataService'];
+        ${camelCaseViz}ToolsLink.$inject = ['scope', 'ele', 'attrs', 'ctrl'];
 
-            return {
-                restrict: 'EA',
-                scope: {},
-                require: ['^toolPanel'],
-                controllerAs: '${camelCaseViz}Tools',
-                bindToController: {},
-                templateUrl: 'widgets/${dir}/${vizName}-tools.directive.html',
-                controller: ${camelCaseViz}ToolsCtrl,
-                link: ${camelCaseViz}ToolsLink
-            };
+        return {
+            restrict: 'EA',
+            scope: {},
+            require: ['^toolPanel'],
+            controllerAs: '${camelCaseViz}Tools',
+            bindToController: {},
+            templateUrl: 'widgets/${dir}/${vizName}-tools.directive.html',
+            controller: ${camelCaseViz}ToolsCtrl,
+            link: ${camelCaseViz}ToolsLink
+        };
 
-            function ${camelCaseViz}ToolsCtrl($rootScope, $scope, dataService) {
-                var ${camelCaseViz}Tools = this;
+        function ${camelCaseViz}ToolsCtrl($rootScope, $scope, dataService) {
+            var ${camelCaseViz}Tools = this;
 
-                ${camelCaseViz}Tools.updateVisualization = updateVisualization;
+            ${camelCaseViz}Tools.updateVisualization = updateVisualization;
 
-                /**
-                 * @name updateVisualization
-                 * @desc generic function that requires the fn passed in to be a function in the directive
-                 */
-                function updateVisualization(fn, data) {
-                    $scope.toolPanelCtrl.updateState(fn, data);
-                    $scope.toolPanelCtrl.runState();
-                }
-            }
-
-            function ${camelCaseViz}ToolsLink(scope, ele, attrs, ctrl) {
-                //declare/initialize scope variables
-                scope.toolPanelCtrl = ctrl[0];
+            /**
+             * @name updateVisualization
+             * @desc generic function that requires the fn passed in to be a function in the directive
+             */
+            function updateVisualization(fn, data) {
+                $scope.toolPanelCtrl.updateState(fn, data);
+                $scope.toolPanelCtrl.runState();
             }
         }
-    })();`;
+
+        function ${camelCaseViz}ToolsLink(scope, ele, attrs, ctrl) {
+            //declare/initialize scope variables
+            scope.toolPanelCtrl = ctrl[0];
+        }
+    }
+})();`;
     fs.writeFile(`${vizName}-tools.directive.js`, toolsDirective, (err) => {
         if (err) {
             throw err;
@@ -507,7 +507,7 @@ if (additionalTools) {
     let toolsHTML = 
     `<div class="grid12">
 
-    </div>`;
+</div>`;
     fs.writeFile(`${vizName}-tools.directive.html`, toolsHTML, (err) => {
         if (err) {
             throw err;
