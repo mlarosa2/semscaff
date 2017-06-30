66f66d7af1147b24147dd38a96babaee3790f7ba
LaRosa <mlarosa@deloitte.com>
2017-06-30 15:42:18 -0400

    begin building out VizBuilder

--- classes/VizBuilder.js
+++ classes/VizBuilder.js
@@ -2,7 +2,16 @@ const rl = require('readline-sync');
 
 class VizBuilder {
     constructor() {
-
+        this.vizname;
+        this.icon;
+        this.dependencies;
+        this.removeDupes;
+        this.configObj;
+        
+        this.tags   = ["Visualization"];
+        this.modes  = ["default-mode"];
+        this.fields = [];
+        this.state  = {};
     }
 
 
