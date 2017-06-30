6e96aab7d226cacb743d474c45160ac568f85650
LaRosa <mlarosa@deloitte.com>
2017-06-30 15:53:15 -0400

    finish constructor for VizBuilder

--- classes/VizBuilder.js
+++ classes/VizBuilder.js
@@ -2,13 +2,18 @@ const rl = require('readline-sync');
 
 class VizBuilder {
     constructor() {
+        this.configObj;
         this.vizname;
         this.icon;
         this.dependencies;
         this.removeDupes;
-        this.configObj;
         
-        this.tags   = ["Visualization"];
+        this.tags   = ['Visualization'];
+        this.contextMenu = {
+            'Change Visualization Type': ['visual-panel-dimensions'],
+            'Additional Tools': [],
+            'Save / Share / Export': ['save', 'share', 'embed-link', 'export-csv', 'export-jpeg', 'expor-svg', 'tableau-connect']
+        };
         this.modes  = ["default-mode"];
         this.fields = [];
         this.state  = {};
