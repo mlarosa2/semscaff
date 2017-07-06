3706836f68f3a0d2707bd41b68067b7f81504d85
Mark LaRosa <mark.larosa2@gmail.com>
2017-07-06 16:48:20 -0400

    remove unused libs

--- entry.js
+++ entry.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
-const rl      = require('readline-sync');
-const fs      = require('fs');
 const process = require('process');
 const Main    = require('./classes/Main.js');
-
-const main = new Main(process.argv[2]);
+const main    = new Main(process.argv[2]);
 
 main.initialize();
